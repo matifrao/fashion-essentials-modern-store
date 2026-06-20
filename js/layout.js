e1e01ddbccc3c968c2794a99536ae4cdbecfb17c
Muhammad Atif Qayyum <matifrao@gmail.com>
2026-06-20 22:21:50 +0500

    Sync local project

---
+++ js/layout.js
@@ -0,0 +1,13 @@
+// Navbar
+fetch("components/navbar.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
+
+// Footer
+fetch("components/footer.html")
+  .then(res => res.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  });
